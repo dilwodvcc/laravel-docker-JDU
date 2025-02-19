95a2350fc32754125bdd63af2f042ceafb4a1b06
dilwodvc <abdullokh207@example.com>
2025-02-19 20:34:02 +0500

    Models complete :)

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Group;
+use App\Models\Room;
+use App\Models\Student;
+use App\Models\Subject;
+use App\Models\Teacher;
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Database\Factories\StudentFactory;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // User::factory(10)->create();
+
+//        User::factory()->create([
+//            'name' => 'Test User',
+//            'email' => 'test@example.com',
+//        ]);
+        Student::factory()->count(10)->create();
+        Teacher::factory()->count(10)->create();
+        Room::factory()->count(10)->create();
+        Group::factory()->count(10)->create();
+        Subject::factory()->count(10)->create();
+    }
+}
